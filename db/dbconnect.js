a6d1a83406386cc1e60d960f37c7cb8ffd9f922d
sanjaysingh24 <sanjubora84@gmail.com>
2025-11-09 20:02:54 +0530

    update

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async()=>{
     try{
-     const conn = await mongoose.connect('mongodb://localhost:27017/swaggerdb')
+     const conn = await mongoose.connect('mongodb+srv://sanjay:sanjay@cluster0.rkqiztu.mongodb.net/swaggerdb')
      if(conn){
         console.log("DB connected successfully");
 
