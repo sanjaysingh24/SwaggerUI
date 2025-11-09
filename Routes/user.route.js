4a4062882226095fdfa2464593cffb2629a98573
sanjaysingh24 <sanjubora84@gmail.com>
2025-11-09 20:19:04 +0530

    update

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createuser, getusers } from '../controllers/user.controller.js';
+import { createuser, getuserbyid, getusers } from '../controllers/user.controller.js';
 import { validate } from '../middleware/validation.js';
 import { userRegisterSchema } from '../utils/Schema.js';
 export const Userrouter = express.Router();
 
 Userrouter.post('/register',validate(userRegisterSchema),createuser)
 .get('/users',getusers)
+.get('/users/:id',getuserbyid);
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -32,3 +32,16 @@ export const getusers = async(req,res)=>{
         res.status(500).json({message:"Internal Server Error", error: err.message});
     }
 }
+export const getuserbyid = async(req,res)=>{
+    const {id} = req.params;
+    try{
+        const user = await User.findById(id).select('-password');
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        res.status(200).json({user});
+    }catch(err){
+        res.status(500).json({message:"Internal Server Error", error: err.message});
+    }
+
+}
